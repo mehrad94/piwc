ce956ee540d7af8b9b02a93778c982b544a7d7d2
Mehrad <Mehradfarahnak9@gmail.com>
2025-01-30 17:20:07 +0330

    version 1.0.0

--- src/components/OurYoutube/OurYoutubeContainer.tsx
+++ src/components/OurYoutube/OurYoutubeContainer.tsx
@@ -4,6 +4,7 @@ export const OurYoutubeContainer = async () => {
   try {
     const response = await fetch('http://localhost:3000/api/youtube/playlist')
     const playlist = await response.json()
+    console.log({ playlist })
     return <OurYoutube playlists={playlist.data.items} />
   } catch (error) {
     console.log({ error })
